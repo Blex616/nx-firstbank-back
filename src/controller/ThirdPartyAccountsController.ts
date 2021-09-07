638e55cd25606e2341756d2b840ed51c766ed96e
Julian Iglesias <julian-14-12@hotmail.com>
2021-09-07 18:48:00 -0500

    Validacion cuenta existente

--- src/controller/ThirdPartyAccountsController.ts
+++ src/controller/ThirdPartyAccountsController.ts
@@ -1,10 +1,12 @@
 import { getRepository } from "typeorm";
 import { NextFunction, Request, Response } from "express";
 import { ThirdPartyAccounts } from "../entity/ThirdPartyAccounts";
+import { Account } from "../entity/Accounts";
 
 export class ThirdPartyAccountsController {
 
     private thirdPartyAccounts = getRepository(ThirdPartyAccounts);
+    private accountRepository = getRepository(Account);
 
     async thirdPartyAccountsUser(request: Request, response: Response, next: NextFunction) {
         return this.thirdPartyAccounts.find({ user: request.body.user });
@@ -12,10 +14,15 @@ export class ThirdPartyAccountsController {
 
     async save(request: Request, response: Response, next: NextFunction) {
         try {
+            const account = await this.accountRepository.findOne({ AcNumber: request.body.accountNumber })
             const validateAccount = await this.thirdPartyAccounts.findOne({ user: request.body.user, accountNumber: request.body.accountNumber })
             if (validateAccount) {
                 response.status(400);
                 return { "message_error": `El número de cuenta ${request.body.accountNumber} ya existe con el alias ${validateAccount.alias}` }
+            }
+            if (!account) {
+                response.status(400);
+                return { "message_error": `El número de cuenta ${request.body.accountNumber} no existe en nuestra base de datos, no es posible registrarla` }
             }
             let thirdPartyAccountsUser = new ThirdPartyAccounts();
             thirdPartyAccountsUser.alias = request.body.alias;
